9cca495a0561258eebf016d2354f5fe62d24a87b
Purabi Nandi <90175857+Nandi-Purabi@users.noreply.github.com>
2021-09-24 00:11:37 +0530

    updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var div=document.createElement("div");
 div.style.cssText='text-align:center;position: absolute;top: 50%;left: 50%;-ms-transform: translateX(-50%) translateY(-50%);-webkit-transform: translate(-50%,-50%);transform: translate(-50%,-50%);';
 var h1=document.createElement("h1");
-h1.style.cssText='text-align:center;font-family:Comic Sans MS;font-size:130px;';
+h1.style.cssText='text-align:center;font-family:Comic Sans MS;font-size:100px;';
 div.appendChild(h1);
 document.body.appendChild(div);
 
